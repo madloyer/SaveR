050380f0d409ec63a31a7aa15ed60036d4819bf7
madloyer <87596107+madloyer@users.noreply.github.com>
2024-01-13 18:13:51 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,16 +12,16 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 14426508
+API_HASH = "e44bc81e970faf1a15fea053d02c6cb7"
+BOT_TOKEN = "6494673462:AAFLjjxGOYxDyuShBKVLP6ZQD_HyNyg533o"
+SESSION = "1AZWarzkBu2HCnFcH9qCMPk8ZxEbWfSb3CdI340C36y9PZr5fQTmZSWVxx_7UEmjbvA6FLo_hE-HENLeuIdzJjijyeq1Lce9Ps8dDCnFhmiXx6isxhEehRwkg2GmMgGabaN-eQiHvqN1dlNsfv8tgmqgBDUC4TEbw1niKtw2i8hpZHVJXXcWnzjoO-pmiGwSgSfvuH_ZR6cX-YmDjZ3wH7iefNMhUnUZV1_T8OvqXQYGYeZruUJvr1JuRX0a5XBl4sJjqr45VikN-vaXxk9pWQPjze0w0UaP8QFHKAfVNhBisWx-NrTglwuE6MImbDzbEfLTinDNWTzYkZHso9nUypbkABlBFfco="
+FORCESUB = "mantapvids"
+AUTH = 5102999288
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
+userbot = Client("saverkontenbot", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
 try:
     userbot.start()
@@ -30,7 +30,7 @@ except BaseException:
     sys.exit(1)
 
 Bot = Client(
-    "SaveRestricted",
+    "saverkontenbot",
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
     api_hash=API_HASH
